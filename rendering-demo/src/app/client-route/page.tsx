00592fe178ce399afa42197c13855244d9e4c0d6
יגל ארביב <yagelarbiv3@gmail.com>
2024-07-14 12:02:23 +0300

    starting Client Only Code

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { useTheme } from "@/components/theme-provider";
-
-
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoutePage() {
   const theme = useTheme();
+  const result = clientSideFunction();
   return (
-    <h1 style={{ color: theme.colors.primary }}>
-      Client route
+    <h1>
+      Client route{" "}{result}
     </h1>
   );
 };
